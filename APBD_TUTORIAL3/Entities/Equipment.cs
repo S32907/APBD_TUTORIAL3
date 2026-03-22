37c84af01d0601ae25367a5bc692f286b54b1e8e
Oleksandr Iievliev <s32907@pjwstk.edu.pl>
2026-03-22 20:55:00 +0100

    "Added abstract class Equipment to constrcut future Equipmet classes"

---
+++ APBD_TUTORIAL3/Entities/Equipment.cs
@@ -0,0 +1,28 @@
+﻿namespace APBD_TUTORIAL3.Entities;
+
+public abstract class Equipment
+{
+    //fields
+    private static int _id = 0;
+    
+    //properties
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsAvailable { get; set; }
+    public string Description { get; set; }
+    
+    //constructor
+    public Equipment(string name, string description)
+    {
+        Id = ++_id;
+        this.Name = name;
+        this.Description = description;
+        this.IsAvailable = true;
+    }
+    
+    //methods
+    public override string ToString()
+    {
+        return $" Id - {Id} \n Name - {Name} \n IsAvailable - {IsAvailable} \n Description - {Description} \n";
+    }
+}
