4d826d2a18b75da82ed2e3a79270e729ede8e884
Oleksandr Iievliev <s32907@pjwstk.edu.pl>
2026-03-22 15:05:43 +0100

    "added abstract User class to construct future user classes"

---
+++ APBD_TUTORIAL3/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿namespace APBD_TUTORIAL3.Entities;
+using APBD_TUTORIAL3.Enums;
+    
+public abstract class User
+{
+    //properties
+    public string Identifier {get; set;}
+    public string FirstName { get; set; } 
+    public string LastName { get; set; }
+    public UserType Type { get; set; }
+    
+    //constructor
+    public User(string identifier, string firstName,
+        string lastName, UserType type)
+    {
+        Identifier = identifier;
+        FirstName = firstName;
+        LastName = lastName;
+        Type = type;
+    }
+}
