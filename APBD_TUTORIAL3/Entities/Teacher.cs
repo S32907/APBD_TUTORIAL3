c70aab2c663ed7b86d673a530e801a62f36fd590
Oleksandr Iievliev <s32907@pjwstk.edu.pl>
2026-03-22 15:35:05 +0100

    "Added Teacher Class"

---
+++ APBD_TUTORIAL3/Entities/Teacher.cs
@@ -0,0 +1,29 @@
+﻿namespace APBD_TUTORIAL3.Entities;
+using APBD_TUTORIAL3.Enums;
+public class Teacher : User
+{
+    
+    //field
+    private double _popularityRating;
+    //properties
+    public double PopularityRating 
+    {
+        get {return _popularityRating;}
+        set 
+        {
+            if (value < 0 || value > 5)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "PopularityRating must be in range from 0 to 5.");
+            }
+            _popularityRating = value;
+        }
+    }
+    
+    //constructor
+    public Teacher(string identifier, string firstName,
+        string lastName, double popularityRating) : base(identifier, firstName, lastName, UserType.Teacher)
+    {
+        PopularityRating = popularityRating;
+    }
+    
+}
