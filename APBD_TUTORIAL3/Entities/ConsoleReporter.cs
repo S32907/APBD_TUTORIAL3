0a782d2a9f3168eb998f4bdd50d16848833b397f
Oleksandr Iievliev <s32907@pjwstk.edu.pl>
2026-03-23 22:14:11 +0100

    "added ConsoleReporter.cs as info displayer and RentalService.cs as logic"

---
+++ APBD_TUTORIAL3/Entities/ConsoleReporter.cs
@@ -0,0 +1,64 @@
+﻿namespace APBD_TUTORIAL3.Entities;
+
+public class ConsoleReporter: IConsoleReporter
+
+{
+    
+    //properties
+    public RentalService RentalService { get; set;}
+
+    public ConsoleReporter(RentalService rentalService)
+    {
+        this.RentalService = rentalService;
+    }
+    
+    public void DisplayEquipment()
+    {
+        Console.WriteLine("List of equipments:");
+        List<Equipment> equipments = RentalService.GetEquipments();
+        foreach (Equipment equipment in equipments)
+        {
+            Console.WriteLine(equipment);
+        }
+    }
+
+    public void DisplayAvailableEquipment()
+    {
+        Console.WriteLine("List of available equipments:");
+        List<Equipment> equipments = RentalService.GetAvailableEquipments();
+        foreach (Equipment equipment in equipments)
+        {
+            Console.WriteLine(equipment);
+        }
+    }
+
+    public void DisplayActiveRentalsByUSer(int userId)
+    {   
+        Console.WriteLine("List of active rental for user with ID : " + userId);
+        List<Rental> rentals = RentalService.GetActiveRentalsByUser(userId);
+        foreach (Rental rental in rentals)
+        {
+            Console.WriteLine(rental);
+        }
+    }
+
+    public void DisplayOverDueRentals()
+    {
+        Console.WriteLine("List of over due rentals:");
+        List<Rental> rentals = RentalService.GetOverDueRentals();
+        foreach (Rental rental in rentals)
+        {
+            Console.WriteLine(rental);
+        }
+    }
+
+    public void GenerateShortSummary()
+    {
+        Console.WriteLine("Short summary:");
+        Console.WriteLine("Number of users: " + RentalService.UERepository.Users.Count );
+        Console.WriteLine("Number of equipments: " + RentalService.UERepository.Equipments.Count );
+        Console.WriteLine("Number of available equipments: " + RentalService.GetAvailableEquipments().Count );
+        Console.WriteLine("Number of over due rentals: " + RentalService.GetOverDueRentals().Count );
+        Console.WriteLine();
+    }
+}
---
+++ APBD_TUTORIAL3/Entities/RentalService.cs
@@ -0,0 +1,123 @@
+﻿using APBD_TUTORIAL3.Enums;
+
+namespace APBD_TUTORIAL3.Entities;
+
+public class RentalService: IEquipmentService, IUserService, IRentalService
+{
+    //properties
+    public List<Rental> Rentals { get; set; } = new List<Rental>();
+    public UERepository UERepository {get; set;}
+
+    public RentalService(UERepository UERepository)
+    {
+        this.UERepository = UERepository;
+    }
+    
+    public Rental GetRental(int userId, int equipmentId)
+    {
+        foreach (var rental in Rentals)
+        {
+            if (rental.User.UserId == userId && rental.Equipment.Id == equipmentId)
+            {
+                return rental;
+            }
+        }
+        throw new Exception("No rental found");
+    }
+    
+    //IEquipmentService
+    public void AddEquipment(Equipment equipment)
+    {
+        UERepository.AddEquipment(equipment);
+    }
+    
+    public List<Equipment> GetEquipments()
+    {
+        return UERepository.Equipments;
+    }
+
+    public List<Equipment> GetAvailableEquipments()
+    {
+        List<Equipment> result = new List<Equipment>();
+        foreach (var equipment in UERepository.Equipments)
+        {
+            if (equipment.IsAvailable)
+            {
+                result.Add(equipment);
+            }
+        }
+        return result;
+    }
+    
+    //IUserService
+    public void AddUser(User user)
+    {
+        UERepository.AddUser(user);
+    }
+    
+    //IRentalService
+    public List<Rental> GetActiveRentalsByUser(int userId)
+    {
+        List<Rental> rentals = new List<Rental>();
+        foreach (var rental in Rentals)
+        {
+            if (rental.IsActive && rental.User.UserId == userId)
+            {
+                rentals.Add(rental);
+            }
+        }
+        return rentals;
+    }
+    
+    public void RentEquipment(int userId, int equipmentId, DateTime DueDate)
+    {   
+        User user = UERepository.GetUser(userId);
+        if (user.Type == UserType.Student && GetActiveRentalsByUser(userId).Count >= 2)
+        {
+            Console.WriteLine($"Student {user.FirstName} {user.LastName} cannot rent more than 2 equipments");
+            return;
+        }
+        else if (user.Type == UserType.Teacher && GetActiveRentalsByUser(userId).Count >= 5)
+        {
+            Console.WriteLine($"Teacher {user.FirstName} {user.LastName} cannot rent more than 5 equipments");
+            return;
+        }
+        Equipment equipment = UERepository.GetEquipment(equipmentId);
+        if (!equipment.IsAvailable)
+        {
+            Console.WriteLine($"User {user.LastName} {user.LastName} with id {user.UserId} can not rent equipment {equipment.Name} with id {equipment.Id} because it is not available");
+            return;
+        }
+        Rental rental = new Rental(user, equipment, DueDate);
+        Rentals.Add(rental);
+        equipment.IsAvailable = false;
+    }
+
+    public void ReturnEquipment(int userId, int equipmentId, DateTime? returnDate = null)
+    {
+        Rental rental = GetRental(userId, equipmentId);
+        if (returnDate != null)
+        {
+            rental.Return(returnDate.Value);
+        }
+        else
+        {
+            rental.Return();
+        }
+    }
+    
+
+    public List<Rental> GetOverDueRentals()
+    {
+        List<Rental> rentals = new List<Rental>();
+        foreach (var rental in Rentals)
+        {
+            if (rental.DueDate < DateTime.Now && rental.IsActive)
+            {
+                rentals.Add(rental);
+            }
+        }
+        return rentals;
+    }
+    
+}
